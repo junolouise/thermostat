8fc0aa2a188bcca7614ca192442f5a1607bdf186
Student <katierodgers95@gmail.com>
2020-01-24 14:58:52 +0000

    added api for weather local and international

--- jasmine/src/interface.js
+++ jasmine/src/interface.js
@@ -26,10 +26,27 @@ $(document).ready(function() {
     thermostat.powerSavingMode('off');
   })
 
-function updateTemperature() {
-  $('#temperature').text(thermostat.temperature);
-  $('#temperature').attr('class', thermostat.currentEnergyUsage())
-}
+  function updateTemperature() {
+    $('#temperature').text(thermostat.temperature);
+    ('#temperature').attr('class', thermostat.currentEnergyUsage())
+  }
+
+  function displayWeather(city) {
+  var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+  var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+  var units = '&units=metric';
+  $.get(url + token + units, function(data) {
+    $('#current-temperature').text(data.main.temp);
+  })
+  }
+
+  displayWeather('London');
+
+$('#select-city').submit(function(event) {
+  event.preventDefault();
+  var city = $('#current-city').val();
+  displayWeather(city);
+})
 
 })
 
